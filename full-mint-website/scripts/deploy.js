029970e50abd42f6b7287862a69a0045b4b8c950
0xfavouradetona <favouradetona7@gmail.com>
2022-06-11 09:47:56 -0700

    contract done

---
+++ full-mint-website/scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  const FaveTestNFT = await hre.ethers.getContractFactory("FaveTestNFT");
+  const favetestnft = await FaveTestNFT.deploy("Hello, Hardhat!");
+
+  await favetestnft.deployed();
+
+  console.log("FaveTestNFT deployed to:", favetestnft.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
