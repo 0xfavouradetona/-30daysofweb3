847d92dc6a0e221175171b2b893ca2a3826a6be2
Favour Adetona <96779581+0xfavouradetona@users.noreply.github.com>
2022-07-23 17:35:19 -0400

    Update FaveTestNFT.sol

--- full-mint-website/contracts/FaveTestNFT.sol
+++ full-mint-website/contracts/FaveTestNFT.sol
@@ -1145,12 +1145,12 @@ contract Collection is ERC721Enumerable, Ownable {
     using Strings for uint256;
     string public baseURI;
     string public baseExtension = ".json";
-        uint256 public cost = 0.001 ether;
+        uint256 public cost = 0.0001 ether;
     uint256 public maxSupply = 100000;
     uint256 public maxMintAmount = 5;
     bool public paused = false;
 
-    constructor() ERC721("Net2Dev NFT Collection", "N2D") {}
+    constructor() ERC721("FaveTestNFT", "FTN") {}
         // internal
         function _baseURI() internal view virtual override returns (string memory) {
         return "ipfs://EE5MmqVp5MmqVp7ZRMBBizicVh9ficVh9fjUofWicVh9f/";
